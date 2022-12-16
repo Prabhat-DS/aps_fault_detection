f3d37a8a2db11b6f091808feb85edf1d00f2950a
Prabhat-DS <prabhatkumar.18811@gmail.com>
2022-12-16 16:54:44 +0530

    completed model training part

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -17,8 +17,11 @@ class DataTransformationArtifact:
     transformed_test_path: str
     target_encoder_path: str
 
+@dataclass
+class ModelTrainerArtifact:
+    model_path: str
 
-class ModelTrainerArtifact:...
+    
 class ModelEvaluationArtifact:...
 class ModelPusherArtifact:...
 
